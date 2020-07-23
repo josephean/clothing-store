13d9cb37b783d62aadf66d27997f551a43b10c97
josephean <41093639+josephean@users.noreply.github.com>
2020-07-22 21:24:29 -0700

    more hooks

--- metropolitan/src/components/SearchList.js
+++ metropolitan/src/components/SearchList.js
@@ -4,34 +4,26 @@ import SearchItem from './SearchItem';
 import { Link } from 'react-router-dom';
 
 const NO_RESULTS = 'No results found';
-class SearchList extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.close = this.close.bind(this);
+const SearchList = props => {
+ 
+  const close = (e) => {
+    props.onClose(e);
   }
 
-  close = (e) => {
-    this.props.onClose(e);
-  }
+  if (props.items.length) {
+    const listItems = props.items.map((item) => 
+      <Link 
+      to={"/item/" + item.id}
+      key={item.id.toString()} 
+      onClick={(e) => close(e)} 
+      className="item-link" 
+      >
+        <SearchItem id={item.id.toString()} value={item}/>
+      </Link>);
 
-  render() {
-    if (this.props.items.length) {
-      const listItems = this.props.items.map((item) => 
-        <Link 
-        to={"/item/" + item.id}
-        key={item.id.toString()} 
-        onClick={(e) => this.close(e)} 
-        className="item-link" 
-        >
-          <SearchItem id={item.id.toString()} value={item}/>
-        </Link>);
-
-      return listItems; 
-    }
-    else return (<p>{NO_RESULTS}</p>);
+    return listItems; 
   }
+  else return (<p>{NO_RESULTS}</p>);
 }
 
 export default SearchList;
